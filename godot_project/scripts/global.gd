90be7c9d5f523a8372b0a41c3a7ae4d6086e9159
Grosskopf <gerrit@familiegrosskopf.de>
2017-11-05 01:00:22 +0100

    added interface

--- godot_project/scripts/global.gd
+++ godot_project/scripts/global.gd
@@ -27,6 +27,7 @@ var time_for_spot_to_fall = randi()%121
 var time_elapsed_for_spot_to_fall = 0.0
 
 func _process(delta):
+	time+=delta
 	time_elapsed_for_spot_to_fall += delta
 	if time_elapsed_for_spot_to_fall > time_for_spot_to_fall:
 		var spot_to_break = randi()%spots.size()
